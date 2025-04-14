b3dcd14b76af604d3184e1ee85b6fceb0a441d79
Ben Liu <benliu2345@gmail.com>
2025-04-13 17:01:42 -0700

    new changes

--- src/server.cpp
+++ src/server.cpp
@@ -15,14 +15,11 @@ int main(int argc, char **argv) {
   
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
-
-   Uncomment this block to pass the first stage
-  
-   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-   if (server_fd < 0) {
-    std::cerr << "Failed to create server socket\n";
-    return 1;
-   }
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+      std::cerr << "Failed to create server socket\n";
+      return 1;
+  }
   
    // Since the tester restarts your program quite often, setting SO_REUSEADDR
    // ensures that we don't run into 'Address already in use' errors
